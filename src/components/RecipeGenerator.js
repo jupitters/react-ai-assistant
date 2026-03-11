2cce58e4ba8ad5cb89d74591c73c3e1f070e9e8e
jupitters <sampaiobale@gmail.com>
2026-03-11 18:38:15 -0300

    recipe component

--- src/components/RecipeGenerator.js
+++ src/components/RecipeGenerator.js
@@ -6,12 +6,17 @@ const RecipeGenerator = () => {
   const [dietaryRestrictions, setDietaryRestrictions] = useState('')
   const [recipe, setRecipe] = useState('')
 
+  const handleGenerateRecipe = async (ingredients, cuisine, dietaryRestrictions) => {
+    
+  }
+
   return (
     <div>
       <h2>Recipe Generator</h2>
       <input type='text' value={ingredients} onChange={(e) => setIngredients(e.target.value)} placeholder='Enter ingredients(comma separated)' />
       <input type='text' value={cuisine} onChange={(e) => setCuisine(e.target.value)} placeholder='Enter cuisine' />
       <input type='text' value={dietaryRestrictions} onChange={(e) => setDietaryRestrictions} placeholder='Enter dietary restrictions' />
+      <button onClick={handleGenerateRecipe}>Generate Recipe</button>
     </div>
   )
 }
