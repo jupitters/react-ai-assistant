427bd091311721999f659c0476e38f5f175f690e
jupitters <sampaiobale@gmail.com>
2026-03-11 10:02:11 -0300

    image generator component

--- src/components/ImageGenerator.js
+++ src/components/ImageGenerator.js
@@ -4,8 +4,14 @@ const ImageGenerator = () => {
     const [prompt, setPrompt] = useState('')
     const [imageUrls, setImageUrls] = useState([])
 
+    
+
   return (
-    <div>ImageGenerator</div>
+    <div className="tab-content">
+      <h2>Generate Image</h2>
+      <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder='Enter prompt for image' />
+      <button onClick={} >Generate Image</button>
+    </div>
   )
 }
 
