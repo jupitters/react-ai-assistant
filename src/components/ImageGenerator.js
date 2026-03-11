93db2c77c277687c1001b4a1cbafa16c529b86fe
jupitters <sampaiobale@gmail.com>
2026-03-10 21:41:18 -0300

    image generator

--- src/components/ImageGenerator.js
+++ src/components/ImageGenerator.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ImageGenerator = () => {
+    const [prompt, setPrompt] = useState('')
+
   return (
     <div>ImageGenerator</div>
   )
