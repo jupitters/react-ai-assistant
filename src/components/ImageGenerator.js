8875becd50d8df37c82ea18807b73a5d0df68436
jupitters <sampaiobale@gmail.com>
2026-03-11 10:09:38 -0300

    image generator component fixed

--- src/components/ImageGenerator.js
+++ src/components/ImageGenerator.js
@@ -15,12 +15,12 @@ const ImageGenerator = () => {
       <button onClick={generateImage} >Generate Image</button>
 
       <div className='image-grid'>
-        {imageUrls.map((url, index) => {
+        {imageUrls.map((url, index) => (
           <img key={index} src={url} alt={`Generated ${index}`} />
-        })}
-        {[...Array(4 - imageUrls.length)].map((_, index) => {
+        ))}
+        {[...Array(4 - imageUrls.length)].map((_, index) => (
           <div key={index + imageUrls.length} className='empty-image-slot'> </div>
-        })}
+        ))}
       </div>
 
     </div>
