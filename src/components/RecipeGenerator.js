8247dafe1278a82d7e76d03a9e9e5604d6fcf5f0
jupitters <sampaiobale@gmail.com>
2026-03-11 18:34:15 -0300

    chat component

--- src/components/RecipeGenerator.js
+++ src/components/RecipeGenerator.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RecipeGenerator = () => {
+  const [ingredients, setIngredients] = useState('')
+  const [cuisine, setCuisine] = useState('')
+  const [dietaryRestrictions, setDietaryRestrictions] = useState('')
+  const [recipe, setRecipe] = useState()
+
   return (
     <div>RecipeGenerator</div>
   )
